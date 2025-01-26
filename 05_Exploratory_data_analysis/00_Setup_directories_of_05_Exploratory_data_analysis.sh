a94d03aa32d0cd883f5668bd534624c71daf8315
TheNeuvillette <161205278+TheNeuvillette@users.noreply.github.com>
2025-01-26 17:01:24 +0100

    Add files via upload

---
+++ 05_Exploratory_data_analysis/00_Setup_directories_of_05_Exploratory_data_analysis.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=setup_directories
+#SBATCH --time=00:01:00
+#SBATCH --cpus-per-task=3
+#SBATCH --mem-per-cpu=200M
+#SBATCH --partition=pibu_el8
+
+mkdir -p 00_Other
+mkdir -p 01_Reformat_Results
